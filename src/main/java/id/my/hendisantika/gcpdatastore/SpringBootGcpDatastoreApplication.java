453b60b1ec039a1a0b546952873eae9e26026d72
hendisantika <hendisantika@yahoo.co.id>
2025-03-10 08:33:09 +0700

    feat: Add Shell Component🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/gcpdatastore/SpringBootGcpDatastoreApplication.java
+++ src/main/java/id/my/hendisantika/gcpdatastore/SpringBootGcpDatastoreApplication.java
@@ -1,13 +1,20 @@
 package id.my.hendisantika.gcpdatastore;
 
+import id.my.hendisantika.gcpdatastore.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.standard.ShellComponent;
 
+@ShellComponent
 @SpringBootApplication
+@RequiredArgsConstructor
 public class SpringBootGcpDatastoreApplication {
+    private final BookRepository bookRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootGcpDatastoreApplication.class, args);
     }
+
 
 }
