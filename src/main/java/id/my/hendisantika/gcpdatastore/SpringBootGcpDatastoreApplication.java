18c847a60d52e9f4d9499fc66ab1c6b006fbf838
hendisantika <hendisantika@yahoo.co.id>
2025-03-10 07:17:11 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/gcpdatastore/SpringBootGcpDatastoreApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.gcpdatastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGcpDatastoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGcpDatastoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/gcpdatastore/SpringBootGcpDatastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.gcpdatastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGcpDatastoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
